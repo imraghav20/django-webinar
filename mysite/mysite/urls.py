409178cb6d2613fc5b3206344ef726798434163f
imraghav20 <raghav20nov@gmail.com>
2020-03-30 13:32:36 +0530

    created complain register page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from complain import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.register, name="register")
 ]
